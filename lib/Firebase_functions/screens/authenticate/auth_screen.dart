a7b62ba44f3bda4068030e27cc3df70bdd6e2698
netesh5 <netesh.paudel@gmail.com>
2022-03-21 20:12:56 +0545

    auth_main_screen

--- lib/Firebase_functions/screens/authenticate/auth_screen.dart
+++ lib/Firebase_functions/screens/authenticate/auth_screen.dart
@@ -1,11 +1,42 @@
 import 'package:firebaseapp/Firebase_functions/screens/authenticate/signin.dart';
 import 'package:flutter/material.dart';
 
-class auth extends StatelessWidget {
+class auth extends StatefulWidget {
   const auth({Key? key}) : super(key: key);
 
   @override
+  State<auth> createState() => _authState();
+}
+
+class _authState extends State<auth> {
+  @override
   Widget build(BuildContext context) {
-    return signin();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              const Text(
+                "All About Firebase",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                child: Image.asset('Assets/Images/firebase_logo.png'),
+                height: 100,
+              ),
+              sign_in_with_email_password(),
+              const SizedBox(
+                height: 20,
+              ),
+              signin(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Firebase_functions/screens/authenticate/signin.dart
+++ lib/Firebase_functions/screens/authenticate/signin.dart
@@ -1,38 +1,68 @@
 // ignore_for_file: deprecated_member_use
-
 import 'package:firebaseapp/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class signin extends StatefulWidget {
-  @override
-  State<signin> createState() => siginState();
+Widget signin() {
+  AuthServices _auth = AuthServices();
+  return RaisedButton(
+    child: const Text("Sign in Anonomuslyl"),
+    onPressed: () async {
+      var result = await _auth.siginAnono();
+      if (result == null) {
+        debugPrint(result);
+      } else {
+        debugPrint("Signed in");
+        debugPrint(result.uid);
+      }
+    },
+  );
 }
 
-class siginState extends State<signin> {
-  AuthServices _auth = AuthServices();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const Text(
-            "Sign in Anonomusly",
-            style: TextStyle(fontSize: 20),
-          ),
-          RaisedButton(
-            child: const Text("Sign in"),
-            onPressed: () async {
-              var result = await _auth.siginAnono();
-              if (result == null) {
-                debugPrint(result);
-              } else {
-                debugPrint("Signed in");
-                debugPrint(result.uid);
-              }
-            },
-          )
-        ],
+Widget sign_in_with_email_password() {
+  bool state = true;
+  return Column(
+    children: [
+      const SizedBox(
+        height: 20,
       ),
-    );
-  }
+      TextFormField(
+        decoration: InputDecoration(
+            labelText: "E-mail",
+            hintText: "Enter your email",
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      TextFormField(
+        obscureText: state,
+        decoration: InputDecoration(
+            suffixIcon: IconButton(
+              icon: Icon(
+                  // ignore: dead_code
+                  state ? Icons.visibility_off : Icons.visibility),
+              onPressed: () {
+                setState() {
+                  state = false;
+                }
+              },
+            ),
+            labelText: "Password",
+            hintText: "Enter your password",
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      // ignore: avoid_unnecessary_containers
+      Container(
+        color: Colors.amberAccent,
+        width: 150,
+        child: TextButton(
+          onPressed: () {},
+          child: const Text("Sign in"),
+        ),
+      )
+    ],
+  );
 }
