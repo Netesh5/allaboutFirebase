d3ae3c584bb20475a84780dacf5c115780910b34
netesh5 <netesh.paudel@gmail.com>
2022-03-24 23:58:59 +0545

    view_list_data

--- lib/Firebase_functions/screens/home/crud.dart
+++ lib/Firebase_functions/screens/home/crud.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebaseapp/Firebase_functions/screens/home/crud_tile.dart';
 import 'package:firebaseapp/model/crud_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,14 +15,12 @@ class _crudState extends State<crud> {
   @override
   Widget build(BuildContext context) {
     final crud = Provider.of<List<Crud>?>(context);
-    if (crud != null) {
-      crud.forEach((element) {
-        debugPrint(element.title);
-        debugPrint(element.task);
-        debugPrint(element.time);
-      });
-    }
+    if (crud == null) {}
 
-    return Container();
+    return ListView.builder(
+        itemCount: crud!.length,
+        itemBuilder: (context, index) {
+          return crudTile(crud: crud[index]);
+        });
   }
 }
---
+++ lib/Firebase_functions/screens/home/crud_tile.dart
@@ -0,0 +1,21 @@
+import 'package:firebaseapp/Firebase_functions/screens/home/crud.dart';
+import 'package:flutter/material.dart';
+import 'package:firebaseapp/model/crud_model.dart';
+
+class crudTile extends StatelessWidget {
+  final Crud? crud;
+  crudTile({this.crud});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListTile(
+          title: Text(crud.title),
+          subtitle: Text(crud.task),
+          trailing: Text(crud.time),
+        ),
+      ),
+    );
+  }
+}
